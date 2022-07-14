d42cef6323bfff16db8296d6b7db6edf98a86554
unknown <saurabhkodam@gmail.com>
2022-07-14 21:37:02 +0530

    Added new Lombok Library for Logging

--- src/main/java/com/example/employeepayrollapplication/EmployeePayrollApplication.java
+++ src/main/java/com/example/employeepayrollapplication/EmployeePayrollApplication.java
@@ -8,10 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class EmployeePayrollApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollApplication.class, args);
         System.out.println("Welcome to Employee Payroll Application");
-        System.out.println();
+        log.info("***************Using Lombok Library for Logging***********************");
     }
 }
