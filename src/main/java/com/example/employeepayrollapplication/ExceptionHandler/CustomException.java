049b73d8b2da820e5763765a15e6e43ee6880f20
unknown <saurabhkodam@gmail.com>
2022-07-16 00:17:13 +0530

    Added Validation for all fields

--- src/main/java/com/example/employeepayrollapplication/ExceptionHandler/CustomException.java
+++ src/main/java/com/example/employeepayrollapplication/ExceptionHandler/CustomException.java
@@ -1,8 +1,10 @@
 package com.example.employeepayrollapplication.ExceptionHandler;
 
 import com.example.employeepayrollapplication.dto.ResponceDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +14,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class CustomException {
+    private String message="Exception while processing REST request";
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponceDTO> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException exception) {
@@ -20,12 +25,18 @@ public class CustomException {
         List<String> errMsg = errorList.stream()
                 .map(objErr -> objErr.getDefaultMessage())
                 .collect(Collectors.toList());
-        ResponceDTO responceDTO = new ResponceDTO("Exception while processing REST request", errMsg);
+        ResponceDTO responceDTO = new ResponceDTO(message,errMsg);
         return new ResponseEntity<>(responceDTO, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponceDTO> handleEmployeePayrollException(EmployeePayrollException exception){
-        ResponceDTO respDTO = new ResponceDTO("Exception while processing REST request",exception.getMessage());
+        ResponceDTO respDTO = new ResponceDTO(message,exception.getMessage());
         return new ResponseEntity<>(respDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponceDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        log.error("Invalid Date Format", exception);
+        ResponceDTO responseDTO = new ResponceDTO(message, "Should have date in the Format dd MM yyyy");
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/employeepayrollapplication/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollapplication/dto/EmployeeDTO.java
@@ -1,22 +1,35 @@
 package com.example.employeepayrollapplication.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
 
+//@Getter
+//@Setter
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDTO {
 
-    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message = "Enter Valid Name..")
+    @NotNull(message = "Employee Name cannot be null")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name Invalid")
     public String name;
-    @NotEmpty(message = "Employee Gender cannot Empty")
-    public String gender;
-    public String department;
     @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
-
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
+    public String gender;
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "startDate should Not be Empty")
+    @PastOrPresent(message = "startDate should be past or todays date")
+    public LocalDate startDate;
+    @NotBlank(message = "Note cannot be Empty")
+    public String note;
+    @NotBlank(message = "profilePic cannot be Empty")
+    public String profilePic;
+    @NotNull(message = "department should Not be Empty")
+    public List<String> department;
 }
+
